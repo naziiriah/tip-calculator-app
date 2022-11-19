0785494a837e386ec2c8a29a2d06807662ee87a2
naziiriah <naziistry7@gmail.com>
2022-11-19 11:28:09 +0100

    branches

--- lib/views/homePage.dart
+++ lib/views/homePage.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: file_names
+// ignore_for_file: file_names, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -12,6 +12,17 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(197, 228, 231, 1),
+      body: SafeArea(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            child: Text("data"),
+          ),
+        ],
+      )),
+    );
   }
 }
